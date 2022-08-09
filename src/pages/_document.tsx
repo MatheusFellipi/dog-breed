8fc83dcbb5eb0042a97d58ca376d43bff074b9be
Matheus Fellipi <matheus.fellipi@outlook.com>
2022-08-09 14:29:44 -0300

    Criando a configuração do estilos globais e fontes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,58 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+import { Global, css } from "@emotion/react";
+const GlobalStyles = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #f8f0f1;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%; /*15px*/
+    }
+    @media (max-width: 720px) {
+      font-size: 87.5%; /*14px*/
+    }
+  }
+
+  body,
+  input,
+  textarea,
+  button {
+    font-family: "Poppins", sans-serif;
+    font-weight: 400;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  strong {
+    font-weight: 600;
+  }
+
+  button {
+    cursor: pointer;
+    transition: all 0.5s;
+    :hover {
+      filter: brightness(0.8);
+    }
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`;
 
 export default class MyDocument extends Document {
   render() {
@@ -15,6 +69,7 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <Global styles={GlobalStyles} />
         </body>
       </Html>
     );
