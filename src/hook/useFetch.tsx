ce95155c7db0d178902417a71f764c28eac3d594
Matheus Fellipi <matheus.fellipi@outlook.com>
2022-08-09 16:31:30 -0300

    Criando o hook de fetch.

---
+++ src/hook/useFetch.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { AxiosRequestConfig } from "axios";
+import { api } from "../services/axios";
+
+export function useFecth<T = unknown>(
+  url: string,
+  options?: AxiosRequestConfig
+) {
+  const [data, setData] = useState<T | null>(null);
+  const [isFetching, setIsFetching] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    api
+      .get(url, options)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setIsFetching(false);
+      });
+  }, []);
+
+  return { data, isFetching, error };
+}
