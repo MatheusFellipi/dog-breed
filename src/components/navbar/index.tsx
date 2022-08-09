e51d78bdd499bbc423aab71ccad6b84b66275559
Matheus Fellipi <matheus.fellipi@outlook.com>
2022-08-09 19:31:43 -0300

    Criando o componentes de cabeçalhos.

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,86 @@
+import { ActiveLink } from "./ActiveLink";
+import styled from "@emotion/styled";
+import Image from "next/image";
+
+export function Navbar() {
+  return (
+    <Header>
+      <div className={"headerContext"}>
+        <div className="logo">
+          <Image src={"/dog-logo.jpg"} width={70} height={70} alt={"logo"} />
+        </div>
+        <nav>
+          <ActiveLink activeClassName={"active"} href="/dashboard">
+            <a>Home</a>
+          </ActiveLink>
+        </nav>
+      </div>
+    </Header>
+  );
+}
+
+const Header = styled.header`
+  height: 5rem;
+
+  .logo {
+    z-index: 50000;
+    margin-right: 10%;
+  }
+
+  .headerContext {
+    max-width: 1520px;
+    height: 5rem;
+    margin: 0 auto;
+    padding: 0 2rem;
+    display: flex;
+    align-items: center;
+
+    &::after {
+      top: 56px;
+      content: " ";
+      height: 2px;
+      border-radius: 3px 3px 0 0;
+      width: 100%;
+      position: absolute;
+      bottom: 1px;
+      left: 0;
+      background-color: #000000;
+    }
+
+    nav {
+      margin-left: 4rem;
+      height: 5rem;
+      a {
+        display: inline-block;
+        position: relative;
+        padding: 0 0.5rem;
+        height: 5rem;
+        line-height: 5rem;
+        color: #707070;
+        transition: color 0.2s;
+        & + a {
+          margin-left: 2rem;
+        }
+        &:hover {
+          color: #3a3a3a;
+          font-weight: bold;
+        }
+        /* &.active::after {
+          content: " ";
+          z-index: 50000;
+          top: 56px;
+          height: 2px;
+          border-radius: 3px 3px 0 0;
+          width: 100%;
+          position: absolute;
+          bottom: 1px;
+          left: 0;
+          background-color: #00caee;
+        } */
+      }
+    }
+    button {
+      margin-left: auto;
+    }
+  }
+`;
