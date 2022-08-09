ce6c8d14b400b790d28b29b489a8d958f6c317b1
Matheus Fellipi <matheus.fellipi@outlook.com>
2022-08-09 15:22:59 -0300

    Alterando o designer do input

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -13,91 +13,94 @@ export function Input({ valeu, onCharge, label, type }: InputProps) {
   }
 
   return (
-    <Label htmlFor={label}>
+    <Label className="form__group field">
       <input
         type={type}
+        className="form__field"
+        placeholder={label}
+        name={label}
         id={label}
-        placeholder="&nbsp;"
         value={valeu}
         onChange={onCharge}
       />
-      <span className="label">{label}</span>
-      <span className="focus-bg"></span>
+      <label htmlFor={label} className="form__label">
+        {label}
+      </label>
     </Label>
   );
 }
 
-const Label = styled.label`
+const Label = styled.fieldset`
+  border: none;
   position: relative;
-  margin: auto;
+  padding: 15px 0 0;
+  margin-top: 10px;
   width: 100%;
-  max-width: 280px;
-  border-radius: 3px;
-  overflow: hidden;
 
-  .label {
-    position: absolute;
-    top: 20px;
-    left: 12px;
-    font-size: 16px;
-    color: rgba($dark, 0.5);
-    font-weight: 500;
-    transform-origin: 0 0;
-    transform: translate3d(0, 0, 0);
-    transition: all 0.2s ease;
-    pointer-events: none;
-  }
-
-  .focus-bg {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    z-index: -1;
-    transform: scaleX(0);
-    transform-origin: left;
-  }
-
-  input {
-    -webkit-appearance: none;
-    appearance: none;
+  .form__field {
+    font-family: inherit;
     width: 100%;
     border: 0;
-    font-family: inherit;
-    padding: 16px 12px 0 12px;
-    height: 56px;
-    font-size: 16px;
-    font-weight: 400;
-    background: rgba($dark, 0.02);
-    box-shadow: inset 0 -1px 0 rgba($dark, 0.3);
-    color: $dark;
-    transition: all 0.15s ease;
-  }
+    border-bottom: 2px solid #9b9b9b;
+    outline: 0;
+    font-size: 1.3rem;
+    color: #fff;
+    padding: 7px 0;
+    background: transparent;
+    transition: border-color 0.2s;
 
-  &:hover {
-    background: rgba($dark, 0.04);
-    box-shadow: inset 0 -1px 0 rgba($dark, 0.5);
-  }
+    &::placeholder {
+      color: transparent;
+    }
 
-  &:not(:placeholder-shown) {
-    + .label {
-      color: rgba($dark, 0.5);
-      transform: translate3d(0, -12px, 0) scale(0.75);
+    &:placeholder-shown ~ .form__label {
+      font-size: 1.3rem;
+      cursor: text;
+      top: 20px;
     }
   }
 
-  &:focus {
-    background: rgba($dark, 0.05);
-    outline: none;
-    box-shadow: inset 0 -2px 0 $primary;
-    + .label {
-      color: $primary;
-      transform: translate3d(0, -12px, 0) scale(0.75);
+  .form__label {
+    position: absolute;
+    top: 0;
+    display: block;
+    transition: 0.2s;
+    font-size: 1rem;
+    color: #9b9b9b;
+  }
+
+  .form__field:focus {
+    ~ .form__label {
+      position: absolute;
+      top: 0;
+      display: block;
+      transition: 0.2s;
+      font-size: 1rem;
+      color: #11998e;
+      font-weight: 700;
     }
-    + .focus-bg {
-      transform: scaleX(1);
-      transition: all 0.1s ease;
+    padding-bottom: 6px;
+    font-weight: 700;
+    border-width: 3px;
+    border-image: linear-gradient(to right, #11998e, #38ef7d);
+    border-image-slice: 1;
+  }
+  /* reset input */
+  .form__field {
+    &:required,
+    &:invalid {
+      box-shadow: none;
     }
+  }
+  /* demo */
+  body {
+    font-family: "Poppins", sans-serif;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    font-size: 1.5rem;
+    background-color: #222222;
   }
 `;
