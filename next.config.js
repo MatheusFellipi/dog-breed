44ea31054424d6568f5c6f57173e9928871f37c3
Matheus Fellipi <matheus.fellipi@outlook.com>
2022-08-10 19:29:48 -0300

    Adicionando configuração de domains da img no next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
 
-module.exports = nextConfig
+  images: {
+    domains: ["images.dog.ceo"],
+  },
+};
+
+module.exports = nextConfig;
